843cc94cfd71041c875c3b8e49df3f2fd463cdac
KilianBre <87305837+KilianBre@users.noreply.github.com>
2024-02-07 09:40:59 +0100

    middleware update

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -9,49 +9,53 @@ const routeWithUserAuth = ['/profil', '/contact', '/about', '/createRessource', 
 const routeForModerator = ['/manageComments', '/newRessources', '/newFiles']
 const routeForAdmin = ['/usersList', '/manageCategories', '/stats']
 const routeForSuperAdmin = ['/usersList']
+const { user, setUser }  = useUser();
 
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
        const pathname = request.nextUrl.pathname;
-       let userRole = useUser().user?.role;
+
+       let responseFromAPI = await fetch('http://localhost:3000/api/verifyUser');
 
        try {
-              if (routeWithoutAuth.includes(pathname) && !useUser().user) { //useUser().user?.role == null
-                     console.log('Page accessible aux utilisateurs déconnectés');
-                     return NextResponse.next()
-              }
-              if (routeForEveryone.includes(pathname)) {
-                     console.log('Page accessible à tous');
-                     return NextResponse.next()
-
-              }
-              if (routeWithUserAuth.includes(pathname) && userRole == 'user') {
-                     console.log('Page accessible aux utilisateurs connectés');
-                     return NextResponse.next()
-
-              }
-              if (routeForModerator.includes(pathname) && userRole == 'moderator') {
-                     console.log('Page accessible aux modérateurs');
-                     return NextResponse.next()
-
-              }
-              if (routeForAdmin.includes(pathname) && userRole == 'administrator') {
-                     console.log('Page accessible aux administrateurs');
-                     return NextResponse.next()
-
-              }
-              if (routeForSuperAdmin.includes(pathname) && userRole == 'superadministrator') {
-                     console.log('Page accessible aux super administrateurs');
-                     return NextResponse.next()
-
+              if (!responseFromAPI.ok) {
+                     return NextResponse.redirect(new URL('/home', request.url))
+              } else {
+                     let data = await responseFromAPI.json();
+                   
+                     let userRole = data.role;
+       
+                     if (routeForEveryone.includes(pathname)) {
+                            console.log('Page accessible à tous');
+                            return NextResponse.next()
+       
+                     }
+                     if (routeWithUserAuth.includes(pathname) && userRole == 'user') {
+                            console.log('Page accessible aux utilisateurs connectés');
+                            return NextResponse.next()
+       
+                     }
+                     if (routeForModerator.includes(pathname) && userRole == 'moderator') {
+                            console.log('Page accessible aux modérateurs');
+                            return NextResponse.next()
+       
+                     }
+                     if (routeForAdmin.includes(pathname) && userRole == 'administrator') {
+                            console.log('Page accessible aux administrateurs');
+                            return NextResponse.next()
+       
+                     }
+                     if (routeForSuperAdmin.includes(pathname) && userRole == 'superadministrator') {
+                            console.log('Page accessible aux super administrateurs');
+                            return NextResponse.next()
+       
+                     }
               }
        } catch (error) {
               console.error('Erreur : Accès non autorisé');
               console.error(error);
               return NextResponse.redirect(new URL('/home', request.url))
        }
-
-
 }
 
 export const config = {
