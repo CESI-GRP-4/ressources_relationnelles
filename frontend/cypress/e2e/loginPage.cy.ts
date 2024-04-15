91d4c144dbbb7647e49b2683738d7e44d8ba48a7
Arthur CRAHÉ <crahe.arthur@gmail.com>
2024-04-15 18:49:06 +0200

    empty email & password test

--- frontend/cypress/e2e/loginPage.cy.ts
+++ frontend/cypress/e2e/loginPage.cy.ts
@@ -1,9 +1,10 @@
-describe('Test login champs vide', () => {
-  it('Login avec champs vide', () => {
-    cy.visit('http://localhost:3000/login')
-    cy.get('#logInForm_email').type(' ')
-    cy.get('#logInForm_password').type(' ')
-    cy.get(':nth-child(4) > .ant-row > .ant-col > .ant-form-item-control-input > .ant-form-item-control-input-content > .ant-btn > span').click
-    cy.url().should('eq', 'http://localhost:3000/')
-  })
-})
+describe('Login page', () => {
+       it('Login with empty email and password', () => {
+              cy.visit('http://localhost:3000/connexion');
+              cy.contains('button', 'Se connecter').click();
+
+              // Check if error messages are displayed
+              cy.get('#logInForm_email_help > .ant-form-item-explain-error').and('contain', 'Veuillez entrer votre adresse e-mail');
+              cy.get('#logInForm_password_help > .ant-form-item-explain-error').should('be.visible').and('contain', 'Veuillez entrer votre mot de passe');
+       });
+});
