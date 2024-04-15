dadb3f95d8b29893b7d58fe2bfc222b8c05de5ea
Arthur CRAHÉ <crahe.arthur@gmail.com>
2024-04-15 19:27:47 +0200

    wrong credentials & pipeline

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
        viewportHeight: 1080,
        viewportWidth: 1920,
+       projectId: "asc3u7",
        e2e: {
               setupNodeEvents(on, config) {
                      // implement node event listeners here
--- frontend/cypress/e2e/loginPage.cy.ts
+++ frontend/cypress/e2e/loginPage.cy.ts
@@ -7,4 +7,24 @@ describe('Login page', () => {
               cy.get('#logInForm_email_help > .ant-form-item-explain-error').and('contain', 'Veuillez entrer votre adresse e-mail');
               cy.get('#logInForm_password_help > .ant-form-item-explain-error').should('be.visible').and('contain', 'Veuillez entrer votre mot de passe');
        });
+
+       it('Login with right credentials', () => {
+              cy.visit('http://localhost:3000/connexion');
+              cy.get('#logInForm_email').type('john.doe@example.com');
+              cy.get('#logInForm_password').type('aze');
+              cy.contains('button', 'Se connecter').click();
+              cy.url().should('eq', 'http://localhost:3000/');
+       });
+
+       it('Login with wrong credentials', () => {
+              cy.visit('http://localhost:3000/connexion');
+              cy.get('#logInForm_email').type('ee');
+              cy.get('#logInForm_password').type('ee');
+              cy.contains('button', 'Se connecter').click();
+              cy.url().should('eq', 'http://localhost:3000/connexion');
+
+              // input fields should be empty
+              cy.get('#logInForm_email').should('have.value', '');
+              cy.get('#logInForm_password').should('have.value', '');
+       });
 });
