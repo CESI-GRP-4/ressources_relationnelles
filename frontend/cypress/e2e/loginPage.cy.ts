745787d01a655725d8355af7b74b5a66aed8c08d
Arthur CRAHÉ <crahe.arthur@gmail.com>
2024-04-17 20:24:53 +0200

    test login right credentials

--- frontend/cypress/e2e/loginPage.cy.ts
+++ frontend/cypress/e2e/loginPage.cy.ts
@@ -1,5 +1,6 @@
 describe('Login page', () => {
        it('Login with empty email and password', () => {
+
               cy.visit('http://localhost:3000/connexion');
               cy.contains('button', 'Se connecter').click();
 
@@ -8,13 +9,13 @@ describe('Login page', () => {
               cy.get('#logInForm_password_help > .ant-form-item-explain-error').should('be.visible').and('contain', 'Veuillez entrer votre mot de passe');
        });
 
-       // it('Login with right credentials', () => {
-       //        cy.visit('http://localhost:3000/connexion');
-       //        cy.get('#logInForm_email').type('john.doe@example.com');
-       //        cy.get('#logInForm_password').type('aze');
-       //        cy.contains('button', 'Se connecter').click();
-       //        cy.url().should('eq', 'http://localhost:3000/');
-       // });
+       it('Login with right credentials', () => {
+              cy.visit('http://localhost:3000/connexion');
+              cy.get('#logInForm_email').type('john.doe@example.com');
+              cy.get('#logInForm_password').type('aze');
+              cy.contains('button', 'Se connecter').click();
+              cy.url().should('eq', 'http://localhost:3000/');
+       });
 
        it('Login with wrong credentials', () => {
               cy.visit('http://localhost:3000/connexion');
