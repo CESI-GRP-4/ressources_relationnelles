27db7bc4c7b60d5132fc8e40e4327e2fb7cddfbb
Arthur CRAHÉ <crahe.arthur@gmail.com>
2024-05-01 12:42:43 +0200

    remove unused pages

--- frontend/src/app/(connected)/(front-office)/a-propos/page.tsx
+++
@@ -1,9 +0,0 @@
-// /a-propos/page.tsx
-
-export default function AboutPage() {
-       return (
-              <div>
-                     <h1>{`Page "A propos"`}</h1>
-              </div>
-       );
-}
