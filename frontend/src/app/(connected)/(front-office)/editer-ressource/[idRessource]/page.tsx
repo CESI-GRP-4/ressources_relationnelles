99958da42af0de2ac2e41a0caaaf73d94bd25f19
Arthur CRAHÉ <crahe.arthur@gmail.com>
2024-05-01 13:20:34 +0200

    feeback on ressource status

--- frontend/src/app/(connected)/(front-office)/editer-ressource/[idRessource]/page.tsx
+++ frontend/src/app/(connected)/(front-office)/editer-ressource/[idRessource]/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useEffect, useState } from 'react';
-import { message, Form, Input, Button, Switch, Select } from 'antd';
+import { message, Form, Input, Button, Switch, Select, Alert } from 'antd';
 import axios, { AxiosError, AxiosResponse } from 'axios';
 import Ressource from "@/types/ressource";
 import PageSummary from '@/components/pageSummary';
@@ -8,6 +8,8 @@ import { Category } from '@/types/category';
 const { Option } = Select;
 import { useRouter } from 'next/navigation';
 import { useUser } from '@/providers/userProvider';
+import { Typography } from 'antd';
+const { Text, Title } = Typography;
 
 export default function EditRessource({ params }: { params: { idRessource: number } }) {
        const [form] = Form.useForm();
@@ -15,16 +17,17 @@ export default function EditRessource({ params }: { params: { idRessource: numbe
        const [categories, setCategories] = useState<Category[]>([]);
        const router = useRouter();
        const { user: currentUser } = useUser();
-
+       const [ressource, setRessource] = useState<Ressource>();
+       console.log(ressource?.status)
        useEffect(() => {
-              if(currentUser?.id != undefined && currentUser?.id != null){
+              if (currentUser?.id != undefined && currentUser?.id != null) {
 
                      fetchRessource();
                      fetchCategories();
               }
        }, [currentUser]);
 
-       if(!currentUser) return null;
+       if (!currentUser) return null;
 
        const fetchRessource = async () => {
               try {
@@ -40,6 +43,7 @@ export default function EditRessource({ params }: { params: { idRessource: numbe
                                    router.push('/mes-ressources');
                             } else {
                                    form.setFieldsValue(response.data.ressource);
+                                   setRessource(response.data.ressource);
                             }
                      } else {
                             throw new Error('Error fetching ressource');
@@ -57,7 +61,7 @@ export default function EditRessource({ params }: { params: { idRessource: numbe
                                           break;
                                    case 404:
                                           message.error("Ressource introuvable");
-                                   
+
                                    default:
                                           message.error("Erreur lors de la récupération de la ressource");
                             }
@@ -172,6 +176,67 @@ export default function EditRessource({ params }: { params: { idRessource: numbe
                             title="Editer une ressource"
                             description="Editer une ressource car elle a été rejettée par un modérateur, ou pour la mettre à jour. Elle repassera en attente de validation et ne sera plus accessible tant qu'elle ne sera pas accéptée par un modérateur."
                      />
+                     {ressource?.status === "blocked" && (
+                            <Alert
+                                   message={<Title level={4}>{`Ressource bloquée`}</Title>}
+                                   description={
+                                          <Text>
+                                                 {`La ressource a été bloquée par un modérateur. Vous ne pouvez plus la modifier.`}{' '}
+                                                 {ressource?.staffComment
+                                                        ? <Text>La ressource a été bloquée pour la raison suivante : <Text strong>{ressource.staffComment}</Text></Text>
+                                                        : 'Aucun commentaire supplémentaire fourni par le staff.'}
+                                          </Text>
+
+                                   }
+                                   type="error"
+                                   showIcon
+                            />
+                     )}
+
+                     {ressource?.status === "rejected" && (
+                            <Alert
+                                   message={<Title level={4}>{`Ressource refusée`}</Title>}
+                                   description={
+                                          <Text>
+                                                 {`La ressource a été refusée par un modérateur. Vous devez la modifier pour qu'elle soit ressoumise à une nouvelle vérification.`}{' '}
+                                                 {ressource?.staffComment
+                                                        ? <Text>{`La ressource a été refusée pour la raison suivante : `}<Text strong>{ressource.staffComment}</Text></Text>
+                                                        : 'Aucun commentaire supplémentaire fourni par le staff.'}
+                                          </Text>
+
+                                   }
+                                   type="warning"
+                                   showIcon
+                            />
+                     )}
+
+                     {ressource?.status === "pending" && (
+                            <Alert
+                                   message={<Title level={4}>{`Ressource en attente`}</Title>}
+                                   description={
+                                          <Text>
+                                                 {`La ressource est en attente de vérification. Vous devez attenndre la vérification d'un modérateur pour qu'elle soit publiée. Vous pouvez toujours modifier votre ressource.`}
+                                          </Text>
+                                   }
+                                   type="info"
+                                   showIcon
+                            />
+                     )}
+
+                     {ressource?.status === "accepted" && (
+                            <Alert
+                                   message={<Title level={4}>{`Ressource acceptée`}</Title>}
+                                   description={
+                                          <Text>
+                                                 {`La ressource a été acceptée par un modérateur. Vous pouvez toujours la modifier. Elle est actuellement visible par la communauté.`}
+                                          </Text>
+                                   }
+                                   type="success"
+                                   showIcon
+                            />
+                     )}
+
+
                      <Form
                             form={form}
                             name="edit_ressource"
@@ -179,13 +244,14 @@ export default function EditRessource({ params }: { params: { idRessource: numbe
                             onFinish={onFinish}
                             autoComplete="off"
                             layout="vertical"
+                            disabled={ressource?.status === "blocked"}
                      >
                             <Form.Item
                                    label="Intitulé"
                                    name="label"
                                    rules={[{ required: true, message: 'Veuillez selectionner un intitulé!' }]}
                             >
-                                   <Input disabled={loading} />
+                                   <Input />
                             </Form.Item>
 
                             <Form.Item
@@ -193,7 +259,7 @@ export default function EditRessource({ params }: { params: { idRessource: numbe
                                    name="description"
                                    rules={[{ required: true, message: 'Veuillez selectionner une description!' }]}
                             >
-                                   <Input.TextArea disabled={loading} rows={4} />
+                                   <Input.TextArea autoSize={{ minRows: 4, maxRows: 10 }} />
                             </Form.Item>
 
                             <Form.Item
