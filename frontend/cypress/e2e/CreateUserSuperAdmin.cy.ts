875acd88fc87246ad8ac656f7cfa4d9c2467ec38
Arthur CRAHÉ <crahe.arthur@gmail.com>
2024-05-15 13:11:26 +0200

    Update CreateUserSuperAdmin.cy.ts

--- frontend/cypress/e2e/CreateUserSuperAdmin.cy.ts
+++ frontend/cypress/e2e/CreateUserSuperAdmin.cy.ts
@@ -1,20 +1,19 @@
 import { loginAsSuperAdmin, generateRandomEmail, generateRandomFirstName, generateRandomLastName } from "./utils";
 
 
-describe('Dashboard page', () => {
-  it('Check if the dashboard is accessible after login', () => {
+describe('User management', () => {
+  it('Create a user from the user management page from back-office', () => {
     loginAsSuperAdmin();
-    cy.visit('http://localhost:3000/tableau-de-bord');
+    cy.visit('http://localhost:3000/gestion-utilisateurs');
 
     // Ensure you are on the tableau-de-bord URL
-    cy.url().should('eq', 'http://localhost:3000/tableau-de-bord');
-    cy.intercept('POST', '/api/gestion-utilisateurs/create').as('createRequest');
+    cy.url().should('eq', 'http://localhost:3000/gestion-utilisateurs');
+    cy.intercept('POST', '/api/user/create').as('createRequest');
 
     const randomEmail = generateRandomEmail();
     const randomFirstName = generateRandomFirstName();
     const randomLastName = generateRandomLastName();
 
-    cy.get('.mr-20 > .ant-btn').click();
     cy.get('.justify-start > :nth-child(2) > .ant-btn').click();
     cy.get('#createUserForm_email').clear().type(randomEmail);;
     cy.get('#createUserForm_firstName').clear().type(randomFirstName);
