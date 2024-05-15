1e1effee10a98f643714d5e44ab09b9790e1d143
KilianBre <87305837+KilianBre@users.noreply.github.com>
2024-05-15 10:39:46 +0200

    Add to utils export function

--- frontend/cypress/e2e/signupPage.cy.ts
+++ frontend/cypress/e2e/signupPage.cy.ts
@@ -1,3 +1,5 @@
+import { generateRandomPassword, generateRandomEmail, generateRandomFirstName, generateRandomLastName } from "./utils";
+
 describe('Signup page', () => {
        it('Signup with empty email and password', () => {
               cy.visit('http://localhost:3000/connexion');
@@ -62,50 +64,3 @@ describe('Signup page', () => {
               cy.wait('@signup').its('response.statusCode').should('eq', 403);
        });
 });
-
-
-function generateRandomEmail() {
-       const randomString = Math.random().toString(36).substring(2, 10);
-       return `test.${randomString}@example.com`;
-}
-
-function generateRandomFirstName() {
-       const firstNames = ['John', 'Jane', 'Alex', 'Emily', 'Chris', 'Katie', 'Michael', 'Sarah', 'David', 'Laura'];
-       return firstNames[Math.floor(Math.random() * firstNames.length)];
-}
-
-function generateRandomLastName() {
-       const lastNames = ['Smith', 'Johnson', 'Williams', 'Jones', 'Brown', 'Davis', 'Miller', 'Wilson', 'Moore', 'Taylor'];
-       return lastNames[Math.floor(Math.random() * lastNames.length)];
-}
-
-export function generateRandomPassword() {
-       const lowerCaseChars = 'abcdefghijklmnopqrstuvwxyz';
-       const upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-       const digits = '0123456789';
-       const specialChars = '@$!%*?&';
-
-       function getRandomChar(chars: string) {
-              return chars.charAt(Math.floor(Math.random() * chars.length));
-       }
-
-       let password = '';
-       password += getRandomChar(lowerCaseChars);
-       password += getRandomChar(upperCaseChars);
-       password += getRandomChar(digits);
-       password += getRandomChar(specialChars);
-
-       const allChars = lowerCaseChars + upperCaseChars + digits + specialChars;
-       for (let i = 4; i < 8; i++) { // Ensuring minimum length of 8 characters
-              password += getRandomChar(allChars);
-       }
-
-       password = password.split('').sort(() => 0.5 - Math.random()).join(''); // Shuffle the password characters
-
-       // If needed, continue adding random characters to meet length requirement beyond 8
-       while (password.length < 12) {
-              password += getRandomChar(allChars);
-       }
-
-       return password;
-}
--- frontend/cypress/e2e/utils.ts
+++ frontend/cypress/e2e/utils.ts
@@ -18,3 +18,50 @@ export const loginAsSuperAdmin = () => {
        // Wait for the login request to complete
        cy.wait('@loginRequest').its('response.statusCode').should('eq', 200);
 }
+
+
+export function generateRandomEmail() {
+       const randomString = Math.random().toString(36).substring(2, 10);
+       return `test.${randomString}@example.com`;
+}
+
+export function generateRandomFirstName() {
+       const firstNames = ['John', 'Jane', 'Alex', 'Emily', 'Chris', 'Katie', 'Michael', 'Sarah', 'David', 'Laura'];
+       return firstNames[Math.floor(Math.random() * firstNames.length)];
+}
+
+export function generateRandomLastName() {
+       const lastNames = ['Smith', 'Johnson', 'Williams', 'Jones', 'Brown', 'Davis', 'Miller', 'Wilson', 'Moore', 'Taylor'];
+       return lastNames[Math.floor(Math.random() * lastNames.length)];
+}
+
+export function generateRandomPassword() {
+       const lowerCaseChars = 'abcdefghijklmnopqrstuvwxyz';
+       const upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+       const digits = '0123456789';
+       const specialChars = '@$!%*?&';
+
+       function getRandomChar(chars: string) {
+              return chars.charAt(Math.floor(Math.random() * chars.length));
+       }
+
+       let password = '';
+       password += getRandomChar(lowerCaseChars);
+       password += getRandomChar(upperCaseChars);
+       password += getRandomChar(digits);
+       password += getRandomChar(specialChars);
+
+       const allChars = lowerCaseChars + upperCaseChars + digits + specialChars;
+       for (let i = 4; i < 8; i++) { // Ensuring minimum length of 8 characters
+              password += getRandomChar(allChars);
+       }
+
+       password = password.split('').sort(() => 0.5 - Math.random()).join(''); // Shuffle the password characters
+
+       // If needed, continue adding random characters to meet length requirement beyond 8
+       while (password.length < 12) {
+              password += getRandomChar(allChars);
+       }
+
+       return password;
+}
