82d068600e17a65784a169472d61d395ff88680a
Tristan13R <98111678+Tristan13R@users.noreply.github.com>
2024-07-04 15:20:43 +0200

    update sentry

--- backend/app/Exceptions/Handler.php
+++ backend/app/Exceptions/Handler.php
@@ -2,7 +2,7 @@
 
 namespace App\Exceptions;
 
-// use Sentry\Laravel\Integration;
+use Sentry\Laravel\Integration;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -25,7 +25,7 @@ class Handler extends ExceptionHandler
     public function register(): void
     {
         $this->reportable(function (Throwable $e) {
-            // Integration::captureUnhandledException($e);
+            Integration::captureUnhandledException($e);
             //
         });
     }
