2e23d30eff1fff4f13db2f45a8e9bad038ab3be8
Arthur CRAHÉ <crahe.arthur@gmail.com>
2024-08-15 23:58:58 +0200

    test test

---
+++ frontend/cypress/e2e/test.cy.ts
@@ -0,0 +1,8 @@
+import { generateRandomPassword, generateRandomEmail, generateRandomFirstName, generateRandomLastName } from "./utils";
+
+describe('Home page pipeline', () => {
+       it('zzzz', () => {
+              cy.visit('http://frontend:3000/');
+              cy.get('h2.ant-typography').contains('(Re)Sources Relationnelles');
+       });
+});
