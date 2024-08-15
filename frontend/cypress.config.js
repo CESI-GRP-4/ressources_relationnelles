736b93d59ab73948a8956e1386bbf4ffb82d224f
Arthur CRAHÉ <crahe.arthur@gmail.com>
2024-08-15 21:02:02 +0200

    Update cypress.config.js

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -8,7 +8,7 @@ module.exports = defineConfig({
     projectId: "asc3u7",
     scrollBehavior: "nearest",
     e2e: {
-       baseUrl: 'http://frontend:3000',
+       baseUrl: 'http://cube_link_github_test-frontend-1:3000',
         setupNodeEvents(on, config) {
             // implement node event listeners here
         },
