47531b3dbc605a9188cf66b8f61d9b8190665aba
Capryc0rne <nico.ch007@gmail.com>
2024-08-22 20:42:00 +0200

    TEST Flemme

--- frontend/cypress/e2e/test.cy.ts
+++ frontend/cypress/e2e/test.cy.ts
@@ -2,7 +2,7 @@ import { generateRandomPassword, generateRandomEmail, generateRandomFirstName, g
 
 describe('Home page pipeline', () => {
        it('zzzz', () => {
-              cy.visit('http://frontend:3001/');
+              cy.visit('http://frontend/');
               cy.get('h2.ant-typography').contains('(Re)Sources Relationnelles');
        });
 });
