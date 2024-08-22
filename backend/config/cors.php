e9219dd2fb2938878179696a8d666169df215b1b
Capryc0rne <nico.ch007@gmail.com>
2024-08-22 21:48:24 +0200

    TEST Flemme :content:

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -12,7 +12,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://cube.aio-tools.com/'],
+    'allowed_origins' => ['https://cube.aio-tools.com/, http://91.108.112.237:3001/'],
 
     'allowed_origins_patterns' => [],
 
