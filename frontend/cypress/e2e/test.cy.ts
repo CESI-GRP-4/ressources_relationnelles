e7ec544c0f26819a1f61965b92d67b4d14dc1449
Capryc0rne <nico.ch007@gmail.com>
2024-08-22 21:11:07 +0200

    TEST Flemme c du troll

--- frontend/cypress/e2e/test.cy.ts
+++ frontend/cypress/e2e/test.cy.ts
@@ -2,7 +2,7 @@ import { generateRandomPassword, generateRandomEmail, generateRandomFirstName, g
 
 describe('Home page pipeline', () => {
        it('zzzz', () => {
-              cy.visit('http://localhost:3001/');
+              cy.visit('http://91.108.112.237:3001/');
               cy.get('h2.ant-typography').contains('(Re)Sources Relationnelles');
        });
 });
