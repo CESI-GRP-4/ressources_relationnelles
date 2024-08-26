d7d48b9ccb0c90f74c3fac55d95561507193821e
Arthur CRAHÉ <crahe.arthur@gmail.com>
2024-08-26 13:54:13 +0200

    loooool

--- frontend/src/components/logInForm.tsx
+++ frontend/src/components/logInForm.tsx
@@ -32,7 +32,7 @@ export default function LogInForm() {
                      const logInResponse: AxiosResponse<LogInResponse> = await axios({
                             method: 'post',
                             baseURL: process.env.NEXT_PUBLIC_BACKEND_API_URL,
-                            url: "/login",
+                            url: "/loginn",
                             data: formData,
                             withCredentials: true,
                             responseType: 'json',
